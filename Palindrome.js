c3a7c96ac6d3b085273ecd61f8cc6f430609e79f
ABhishek Dubey <65311526+dubey748@users.noreply.github.com>
2022-11-15 02:49:05 +0530

    Add files via upload

---
+++ Palindrome.js
@@ -0,0 +1,15 @@
+const palindrome = (s) => {
+    let start=0;
+    let end=s.length-1;
+    while(end>start){
+        if(s[start]!=s[end]){
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+   
+  };
+  console.log(palindrome("level"));
+  
