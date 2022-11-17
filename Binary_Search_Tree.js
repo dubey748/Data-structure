fc766de98ace1a147480747bf630ff31036cbc1d
ABhishek Dubey <65311526+dubey748@users.noreply.github.com>
2022-11-18 02:27:32 +0530

    Add files via upload

---
+++ Binary_Search_Tree.js
@@ -0,0 +1,49 @@
+class Node {
+  constructor(val) {
+    (this.val = val), (this.left = null), (this.right = null);
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  isTreeEmpty() {
+    return this.root === null;
+  }
+
+  makeTree(val) {
+    let newNode = new Node(val);
+    if (this.isTreeEmpty()) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+  insertNode(root, newNode) {
+    if (root.val > newNode.val) {
+      if (root.left === null) {
+        root.left = newNode;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
+}
+let bst = new BST();
+bst.makeTree(10);
+bst.makeTree(5);
+bst.makeTree(3)
+bst.makeTree(8);
+bst.makeTree(25)
+bst.makeTree(15)
+bst.makeTree(28)
+
+console.log(bst);
+//console.log(bst.isTreeEmpty());
